f6fdd7b89b44adf96cba8823bc3a1ff98c730edc
Sadia Rahman <sadiarahmann02@gmail.com>
2025-08-20 00:42:25 +0600

    Add files via upload

---
+++ calculator_switch_case.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    char op;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Enter operator(+,-,*,%):");
+    scanf("%c",&op);
+
+    switch(op) {
+        case 1: printf("Sum = %d\n", a+b); break;
+        case 2: printf("Difference = %d\n", a-b); break;
+        case 3: printf("Product = %d\n", a*b); break;
+        case 4: printf("Quotient = %.2f\n", (float)a/b); break;
+        default: printf("Invalid \n");
+    }
+    return 0;
+}
