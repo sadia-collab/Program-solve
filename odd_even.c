a199edd036d19967d8ce5dffad2acfea69599196
Sadia Rahman <sadiarahmann02@gmail.com>
2025-08-20 12:01:59 +0600

    Update odd_even.c

--- odd_even.c
+++ odd_even.c
@@ -4,16 +4,15 @@ int main() {
     printf("Enter number: ");
     scanf("%d", &num);
 
-    // Using if-else
     if(num % 2 == 0)
         printf("Even\n");
     else
         printf("Odd\n");
 
-    // Using switch
     switch(num % 2) {
         case 0: printf("Even (switch)\n"); break;
         case 1: printf("Odd (switch)\n"); break;
     }
     return 0;
 }
+
