53b2c27e45b5fa374e176c8e6fd8be7317153976
Sadia Rahman <sadiarahmann02@gmail.com>
2025-08-20 12:13:18 +0600

    Update multiplication_table.c

--- multiplication_table.c
+++ multiplication_table.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+int main() {
+    int num, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
+    for(i = 1; i <= 10; i++) {
+        printf("%d × %d = %d\n", num, i, num*i);
+    }
+    return 0;
+}
